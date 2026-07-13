5d54d50d702f26dc94b8bf1bf4b183253ff29a90
Lorenzo <35490909+lm17918@users.noreply.github.com>
2026-07-13 09:57:37 +0200

    Add: majority-element - submission-10

---
+++ Data Structures & Algorithms/majority-element/submission-10.py
@@ -0,0 +1,13 @@
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        if len(nums)==1:
+            return nums[0]
+        hashmap={}
+        treshold= len(nums)/2
+        for n in nums:
+            if n not in hashmap:
+                hashmap[n]=1
+            else:
+                hashmap[n]+=1
+                if hashmap[n]>treshold:
+                    return n 
