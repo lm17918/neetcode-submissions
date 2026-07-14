4fa0de39bca28e8380700e2674a13f66d44e7704
Lorenzo <35490909+lm17918@users.noreply.github.com>
2026-07-14 11:56:28 +0200

    Add: best-time-to-buy-and-sell-stock-ii - submission-2

---
+++ Data Structures & Algorithms/best-time-to-buy-and-sell-stock-ii/submission-2.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        tot=0
+        sellprice=None
+        buyprice=None
+        for i in range(len(prices)-1,-1,-1):
+            if buyprice is None and (sellprice is None or sellprice<prices[i]):
+                sellprice=prices[i]
+            elif buyprice is None or prices[i] <buyprice:
+                buyprice=prices[i]
+            else:
+                tot += sellprice-buyprice
+                sellprice=prices[i]
+                buyprice=None
+        if buyprice is not None:
+            tot += sellprice-buyprice
+        return tot
